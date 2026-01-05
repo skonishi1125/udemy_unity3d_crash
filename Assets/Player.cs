688fdd5ba797ff8b7199bff7c2f8871d56e32327
SKONI_NOTE\watas <skonishi1125@gmail.com>
2026-01-05 15:43:26 +0800

    マウスポインタに向かって砲台が動くように調整

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,15 +5,19 @@ public class Player : MonoBehaviour
     private Rigidbody rb;
 
     [Header("Movement data")]
-    public float moveSpeed;
-    public float rotationSpeed;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotationSpeed;
 
     private float verticalInput;
     private float horizontalInput;
 
-    [Space]
-    public LayerMask whatIsAimMask;
-    public Transform aimTransform;
+    [Header("Tower data")]
+    [SerializeField] private Transform towerTransform;
+    [SerializeField] private float towerRotationSpeed;
+
+    [Header("Aim data")]
+    [SerializeField] private LayerMask whatIsAimMask;
+    [SerializeField] private Transform aimTransform;
 
 
     private void Start()
@@ -25,7 +29,12 @@ public class Player : MonoBehaviour
     void Update()
     {
         UpdateAim();
+        CheckInput();
 
+    }
+
+    private void CheckInput()
+    {
         // w = 1, s = -1...
         verticalInput = Input.GetAxis("Vertical"); // z
         horizontalInput = Input.GetAxis("Horizontal"); // x
@@ -35,16 +44,32 @@ public class Player : MonoBehaviour
         {
             horizontalInput = -Input.GetAxis("Horizontal");
         }
-
-
     }
 
     private void FixedUpdate()
     {
+        ApplyMovement();
+        ApplyBodyRotation();
+        ApplyTowersRotation();
+    }
+
+    private void ApplyTowersRotation()
+    {
+        Vector3 direction = aimTransform.position - towerTransform.position; // 銃からポインタへ向かうベクトルが出る
+        direction.y = 0; // 上下を無視して、水平回転だけしたい
+        Quaternion targetRotation = Quaternion.LookRotation(direction); // 前方向がdirectionになるような回転の定義
+        towerTransform.rotation = Quaternion.RotateTowards(towerTransform.rotation, targetRotation, towerRotationSpeed); // 目標の値に少しずつ近づける。
+    }
+
+    private void ApplyBodyRotation()
+    {
+        transform.Rotate(0, horizontalInput * rotationSpeed, 0);
+    }
+
+    private void ApplyMovement()
+    {
         Vector3 movement = transform.forward * moveSpeed * verticalInput;
         rb.velocity = movement;
-
-        transform.Rotate(0, horizontalInput * rotationSpeed, 0);
     }
 
     private void UpdateAim()
